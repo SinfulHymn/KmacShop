94bd1d4b9c80df7190be21751a926a5a4f60d3f8
Alejandro Rojas <kmachappy@pm.me>
2022-07-27 07:00:38 -0700

    update

--- kmachappy/settings.py
+++ kmachappy/settings.py
@@ -166,4 +166,3 @@ AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
